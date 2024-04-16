f3b7005edf7a22a35ed1201a21249903cc6edb59
sheetuchoudhary <161558244+sheetuchoudhary@users.noreply.github.com>
2024-04-16 13:51:06 +0000

    second

--- problem_1.py
+++ problem_1.py
@@ -2,6 +2,7 @@
 Problem_1
 To Desgin a function with one parameter 
 '''
+#helper function
 def get_name():
     U_name = input("User Name :")
     return U_name
@@ -10,16 +11,19 @@ def get_name():
 
 #main function
 def get_tshirt(brand_name):
-    #helper function
+    #helper function call
     User_name = get_name()
     #list of brand names
     brands = ['Raymond','Puma','Adidas','Zara']
     for i in brands:
+        #checking the brand is available in list of name brands
         if i == brand_name.isupper or brand_name.islower:
             print("Hi",User_name, "the brand you are looking for is available in our store ")
             break
     else:
         print("Hi",User_name, "Unfortunately the brand you are looking for is not available in our store ")
-brand_name = input('Search Or Brand : ')
+#taking brand name to be search 
+brand_name = input('Search for Brand : ')
+#calling function name get_tshirt
 get_tshirt(brand_name)
 
