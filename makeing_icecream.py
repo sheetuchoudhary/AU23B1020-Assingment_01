3e7e8a96b9c3c1bf253656f56348e3983f3e57cc
sheetuchoudhary <161558244+sheetuchoudhary@users.noreply.github.com>
2024-04-17 14:21:27 +0000

    grow

---
+++ makeing_icecream.py
@@ -0,0 +1,11 @@
+'''
+problem_4.2
+'''
+import ice_cream
+print('your fav ice-cream is here !!!')
+print('ice cream with shake ')
+ice_cream.add_topping()
+ice_cream.make_shake()
+
+print('ice cream without shake ')
+ice_cream.add_toppings()
