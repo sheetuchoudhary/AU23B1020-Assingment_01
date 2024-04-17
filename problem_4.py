4423da2ca85a595275ce7c947d7790bffb398a99
sheetuchoudhary <161558244+sheetuchoudhary@users.noreply.github.com>
2024-04-17 14:05:11 +0000

    p4.1

--- problem_4.py
+++ problem_4.py
@@ -3,9 +3,34 @@ Problem_4
 Program for Problem_4
 '''
 def add_topping(scoop_size,toppings):
+    your_icecream =[]
+    if scoop_size =='2':
+        size = 'Large'
+    else :
+        size = 'Small' 
+
+    your_icecream.append(size)
+
+    print("select any three different items !!")
+    print("1.Sprinkles\n2.Hot fudge\n3.Whipped cream\n4.Crushed nuts\nyour choice :") 
+    n=1
+    while (n<4):
+        t= input('selected item : ')
+        if t =='1':
+            toppings.append('Sprinkles')   
+        if t =='2':
+            toppings.append('Hot fudge')
+        if t =='3':
+            toppings.append('Whipped Cream')
+        if t =='4':
+            toppings.append('Crushed Nuts')
+        n+=1
     
-    for i in toppings :
-        print(i)
-scoop_size = input('1.Small\n.2.Medium\n3.Large\nscoop size :')
-toppings = ['l','s','p']   
+    print("Nice Selection !!\nyour selection is here --")
+    print('scoop size : ',size)
+    print(toppings)
+
+  
+scoop_size = input('1.small\n2.large\nscoop size :')
+toppings =[]
 add_topping(scoop_size,toppings)
