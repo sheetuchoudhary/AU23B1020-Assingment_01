c4f0e57ed4eb2bb165eaf64ebed5cdc5312358a1
sheetuchoudhary <161558244+sheetuchoudhary@users.noreply.github.com>
2024-04-17 11:57:29 +0000

    problem_3

--- problem_3.py
+++ problem_3.py
@@ -1,3 +1,17 @@
 '''
 Problem_3
+program for problem_3
 '''
+def get_order(order):
+    for i in order:
+        print('Preparing Your Order - ',i)
+    print('The following orders have been dispatched:')
+    
+    order.reverse()
+    print(order)
+c_orders = ['Dal_Chawal','Sahhi Paneer','Chutney','Tandoori Roti']
+get_order(c_orders)
+    
+
+
+
