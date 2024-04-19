cb8614287e9c05e7994ba8d5a5ee30f6eb386e89
sheetuchoudhary <161558244+sheetuchoudhary@users.noreply.github.com>
2024-04-19 04:41:41 +0000

    go

--- problem_2.py
+++ problem_2.py
@@ -16,33 +16,34 @@ def get_tshirt(brand_name,size = None):
     brands = ['Raymond','Puma','Adidas','Zara']
     #list of size
     b_size = ['m','l','x','xl','xxl']
+    #checking the brand is available in list of name brands
     for i in brands:
-        #checking the brand is available in list of name brands
-        if i == brand_name.isupper or brand_name.islower:
+        if i== brand_name:
             #if user given to check for size_____if size =True
             if size: 
-                for i in b_size:
+                for j in b_size:
                     #if  brand  and size both are available
-                    if i == size :
+                    if j == size :
                         print("Hi",User_name, "the ",size," size of ",brand_name," brand you are looking for is available in our store ")
                         break
-                #if there is no search for size ____if size = False
-                else:
-                    print("Hi",User_name, "the ",brand_name," brand you are looking for is available in our store ")
+                    else:
+                        print("Hi",User_name, "the ",brand_name," brand you are looking for is available in our store ")
             break
         else:
             if size:
-                for i in b_size:
-                    #size is true but not available of that size
-                    if i!= size :
+                for j in b_size:
+                    if j!= size :
                         print("Hi",User_name, "Unfortunately the ",size," size of ",brand_name," brand you are looking for is not available in our store ")
-                        break              
-            #if no command for size
+                    break              
             else:     
                 print("Hi",User_name, "Unfortunately the ",brand_name," brand you are looking for is not available in our store ")
+                break
 
 #taking brand name to be search 
 b_name = input('Search for Brand : ')
 b_size = input("Size (Optional) : " )
 #calling function name get_tshirt
 get_tshirt(b_name,b_size)
+
+#size is true but not available of that size
+#if no command for size
